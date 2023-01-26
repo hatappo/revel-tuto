cdcf7e3a3bda0667926597b6a12fc3168f7aa9b9
Fumihiko Hata <hatappo@users.noreply.github.com>
2023-01-27 02:56:30 +0900

    add hello world

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,5 +9,20 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	greeting := "Aloha World"
+	return c.Render(greeting)
+	// template が明示されない場合、この場合自動で app/views/App/index.html に解決される。
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("Your name is required!")
+	c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	return c.Render(myName)
 }
